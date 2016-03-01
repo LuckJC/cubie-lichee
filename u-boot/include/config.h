30b43abe7224b3b8a8bb5bff6590e05cc5a3759d
huangchao <huangchao@hcn2000.com>
2016-03-01 13:49:21 +0800

    Add git untracked files

--- u-boot/include/config.h
+++
@@ -1,6 +0,0 @@
-/* Automatically generated - do not edit */
-#define CONFIG_BOARDDIR board/allwinner/sun7i-evb
-#include <config_cmd_defaults.h>
-#include <config_defaults.h>
-#include <configs/sun7i.h>
-#include <asm/config.h>
--- u-boot/include/config.mk
+++
@@ -1,5 +0,0 @@
-ARCH   = arm
-CPU    = armv7
-BOARD  = sun7i-evb
-VENDOR = allwinner
-SOC    = sunxi
